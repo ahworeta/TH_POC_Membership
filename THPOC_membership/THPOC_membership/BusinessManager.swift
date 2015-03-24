9dad66a3cff867c7a08714bea12bcb4f6ef56136
Dev <Dev@ITs-MacBook-Pro-2.local>
2015-03-24 16:55:20 -0400

    Add business manager

---
+++ THPOC_membership/THPOC_membership/BusinessManager.swift
@@ -0,0 +1,26 @@
+//
+//  BusinessManager.swift
+//  THPOC_membership
+//
+//  Created by Dev on 3/24/15.
+//  Copyright (c) 2015 Inflexxion. All rights reserved.
+//
+
+import Foundation
+
+
+class BusinessManager
+{
+    
+    func Login(credential: LoginCredential) -> Bool {
+        
+    
+        if (credential.userName == "test" && credential.pin ==  "123")
+        {
+            return true
+        }
+        
+        return false
+        
+    }
+}
--- THPOC_membership/THPOC_membership/ViewController.swift
+++ THPOC_membership/THPOC_membership/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var userNameFT: UITextField!
+    
+    @IBOutlet weak var pinFT: UITextField!
+    
+    @IBOutlet weak var feedbackLb: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +26,18 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func Login(sender: AnyObject) {
+        var bm = BusinessManager()
+        var c = LoginCredential(loginUserName: userNameFT.text, loginPin: pinFT.text)
+        var a = bm.Login(c)
+        if !a {
+            //userNameFT
+            userNameFT.text = ""
+            pinFT.text = ""
+            feedbackLb.text = "Wrong credential, try again"
+            return
+        }
+    }
 
 }
 
